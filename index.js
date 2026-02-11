c8e4764bb1a5a0f06df5e9249f27a24493fac256
shoaibtechs <p200147@pwr.nu.edu.pk>
2026-02-11 06:21:49 +0500

    feat : connection with mongoose

--- index.js
+++ index.js
@@ -1,6 +1,34 @@
 const express =  require("express");
 const app = express();
 const port = 8080;
+const mongoose =  require("mongoose");
+
+async function main()
+{
+
+     await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+
+}
+
+
+main()
+    .then(()=>{
+
+            console.log("Connection Sucessfull");
+
+
+    })
+
+    .catch((err)=>{
+
+            console.log(err);
+
+
+    })
+
+
+
+
 
 
 
