56b3cdc9570bb907a28806e48633f34dacb99933
shoaibtechs <p200147@pwr.nu.edu.pk>
2026-02-11 06:13:49 +0500

    feat : create a server at port 8080

---
+++ index.js
@@ -0,0 +1,12 @@
+const express =  require("express");
+const app = express();
+const port = 8080;
+
+
+
+app.listen(port, ()=>{
+
+    console.log(`The server is listening at port ${port}`);
+
+
+})
